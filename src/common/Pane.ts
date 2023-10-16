92f32ff4a9af48008fcc6174889a2f1f8938ed31
Harry Kodua <harry.kodua@rangeforce.com>
2023-10-16 23:09:37 +0300

    chore: tweak pane always enabled

--- src/common/Pane.ts
+++ src/common/Pane.ts
@@ -1,9 +1,5 @@
-import { Debug } from '/src/common/Debug';
 import { Pane } from 'tweakpane';
 
 export const pane = new Pane({
   title: 'Tweak Pane',
 });
-
-// Only show when hash #debug is present in the URL
-pane.disabled = !Debug.enabled();
